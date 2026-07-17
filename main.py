350de4ed458d67fd3cd5b233adf22b11b5c50eff
Jefferson <jribeiroamorimfilho@gmail.com>
2026-07-17 00:28:23 -0300

    Simple version

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from pathlib import Path
 from subprocess import run
-import shutil
-import pdb
 
 def access_dir(name):
-    anime = Path(f'/home/jeff/Downloads/{name.lower()}/')
-    return anime
+    return Path(f'/home/jeff/Downloads/{name.lower()}/')
 
 def choose_ep():
     ep = input("Which episode? ")
@@ -15,20 +12,17 @@ def watch_input():
     inp = input("What do you want to watch? ")
     return inp
 
-def play(anime,ep,name):
-    for episode in anime:
-        if r'\d' in episode == ep and int(ep) < 10:
-            with open('/home/jeff/.local/bin/vlcrun.txt','w+') as file:
-                file.write(f'vlc /home/jeff/Downloads/{name.lower()}/ep{ep}')
-                run(['chmod', '+x', f'{file}'])
-                run(f'{file}')
-                break
+def play(anime,ep):
+    for episode in anime.iterdir():
+        if episode.name == f'ep{ep}':
+            run(['vlc', episode])
+            break
 
 def main():
     anime = watch_input()
     ep = choose_ep()
     directory = access_dir(anime)
-    play(directory,ep,anime)
+    play(directory,ep)
 
 if __name__ == '__main__':
     main()
