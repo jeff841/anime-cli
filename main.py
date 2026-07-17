dc299a223980c2030cf46b757918c8ab24968f41
Jefferson <jribeiroamorimfilho@gmail.com>
2026-07-16 23:37:48 -0300

    Made a function to play media

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
-print('Hello')
+from pathlib import Path
+from subprocess import run
+import shutil
+import pdb
+
+def access_dir(name):
+    anime = Path(f'/home/jeff/Downloads/{name.lower()}/')
+    return anime
+
+def choose_ep():
+    ep = input("Which episode? ")
+    return ep
+
+def watch_input():
+    inp = input("What do you want to watch? ")
+    return inp
+
+def play(anime,ep):
+    for episode in anime:
+        if r'\d' in episode == ep and int(ep) < 10:
+            with open('/home/jeff/.local/bin/vlcrun.txt','w+') as file:
+                file.write(f'vlc /home/jeff/Downloads/nisekoi/ep{ep}')
+                run(['chmod', '+x', f'{file}'])
+                run(f'{file}')
+                break
+
+def main():
+    anime = watch_input()
+    ep = choose_ep()
+    directory = access_dir(anime)
+    play(directory,ep)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
