7b4c31625f5c5de4e287036c12a26863d8b2040e
Jefferson <jribeiroamorimfilho@gmail.com>
2026-08-06 15:49:43 -0300

    Error handling

--- anime/main.py
+++ anime/main.py
@@ -10,6 +10,7 @@ from argparse import ArgumentParser
 from tkinter import Tk,filedialog
 from platformdirs import user_config_dir
 from shutil import which
+from typing import reveal_type
 
 def watch_input():
     CONFIG_DIR = Path(user_config_dir('anime'))
@@ -18,20 +19,28 @@ def watch_input():
     CONFIG_FILE.touch(exist_ok=True)
     with CONFIG_FILE.open('r+') as f:
         if path.getsize(f.name) == 0:
-            choicesd = ["Enter the directory where your episode files are located","Open file explorer","Exit"]
+            choicesd = ["Enter the directory where your series folders are located","Open file explorer","Exit"]
             selectedd = wrapper(menu,choicesd)
-            if choicesd[selectedd] == 'Enter the directory where your episode files are located':
+            if choicesd[selectedd] == 'Enter the directory where your series folders are located':
                 while True:                
                     inp = input("Directory: ")
                     d = Path(inp)
                     if d.is_dir():
-                        break
+                        for cont in d.iterdir():
+                            if not cont.is_dir():
+                                print("Select the directory in which you store your series folders. The structure should be: \n./\n    anime1/\n        ep1-\n        ep2-\n        ...\n    anime2/\n        ep1-\n        ep2-\n        ...")
+                                return watch_input()
                     print("This directory does not exist")
             elif choicesd[selectedd] == 'Open file explorer':
                 root = Tk()
                 root.withdraw()
                 folder = filedialog.askdirectory(title='Select your directory')
                 d = folder
+                dpath = Path(d)
+                for cont in dpath.iterdir():
+                    if not cont.is_dir():
+                        print("Select the directory in which you store your series folders. The structure should be: \n./\n    anime1/\n        ep1-\n        ep2-\n        ...\n    anime2/\n        ep1-\n        ep2-\n        ...")
+                        return watch_input()
             else:
                 return
             data = {
